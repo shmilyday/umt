f12fadacd0f03d30a2f0bd01333a9b5af54ac909
appleii <nankai@cnic.ac.cn>
2016-05-17 17:46:10 +0800

    temp commit

---
+++ dependencies/install-dep.sh
@@ -0,0 +1,45 @@
+mvn install:install-file -Dfile=c:\kaptcha-{version}.jar
+-DgroupId=com.google.code -DartifactId=kaptcha -Dversion={version}
+-Dpackaging=jar
+
+
+ com.mysql:mysql-connector-java:jar:bin:3.0.14-production,
+
+ com.octo:filter:jar:2.0,
+ com.octo:jcaptcha-alpha:jar:2.0,
+ com.octo:jcaptcha-api:jar:1.0,
+ com.octo:jcaptcha-intergration:jar:2.0,
+
+ com.qq:SDK4J:jar:2.0,
+ com.sun:mail:jar:1.4,
+
+ commons-logging:commons-logging:jar:1.1.0.jboss,
+
+ javax.jms:jms:jar:1.1,
+ javax.servlet:jsp-api:jar:2.2.FR,
+ javax.servlet:servlet-api:jar:3.0.FR,
+
+ net.duckling.falcon:falcon-api:jar:0.3.4.snapshot,
+ net.duckling:clb:jar:6.2.0, 
+ net.duckling:duckling-common:jar:6.0.6,
+ net.duckling:duckling-utils:jar:1.1.5,
+ net.duckling:falcon-xss:jar:0.3.5-SNAPSHOT:
+ net.duckling:uaf-common:jar:0.1.1,
+ net.duckling:version:jar:1.0.0,
+ net.duckling:vmt:jar:7.1.2b,
+ net.duckling:websso:jar:1.0, 
+ smtpcstnet:smtpcstnet-auth:jar:1.0,
+
+ net.sourceforge:pinyin4j:jar:2.5.0, 
+ net.spy:spymemcached:jar:2.8.0, 
+ org.apache.oro:jakarta-oro:jar:2.0.7,
+ org.apache.poi:poi:jar:3.0.1-FINAL-20070705,
+ org.bouncycastle:bcprov-jdk15:jar:129,
+ org.json.simple:json-simple:jar:1.1.2,
+ org.quartz:quartz-all:jar:1.6.5,
+ org.xmlpull:xpp3_min:jar:1.1.4c,
+
+ tebie.applib:apiclient:jar:1.6,
+ weibo4j:weibo4j:jar:2.2,
+
+
